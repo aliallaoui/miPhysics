6c7f58989811690d60455d1fc62dee64282717f2
Ali Allaoui <ali@marsonline.org>
2019-08-21 17:12:07 +0200

    Audio Management class
    using minim, which is also a processing library

---
+++ miPhysics-core/src/main/java/org/micreative/miPhysics/Engine/Sound/PhyUGen.java
@@ -0,0 +1,63 @@
+package org.micreative.miPhysics.Engine.Sound;
+
+import ddf.minim.*;
+import org.micreative.miPhysics.Engine.PhysicalModel;
+
+import java.util.Arrays;
+
+public class PhyUGen extends UGen { //TODO maybe PhyUGen should inherits also PhysicalModel
+
+    protected long step;
+
+    public PhysicalModel getMdl() {
+        return mdl;
+    }
+
+    protected PhysicalModel mdl;
+
+    public void setListeningPoint(String[] listeningPoint) {
+        this.listeningPoint = listeningPoint;
+    }
+
+    protected String[] listeningPoint;
+//    private float    oneOverSampleRate;
+     protected float    audioOut;
+    protected float    currAudio;
+    // public ArrayList <PVector> modelPos;
+//    public ArrayList <PVector> modelVel;
+
+    float prevSample;
+
+    public PhyUGen(int sampleRate,int baseFrameRate) {
+        super();
+
+        this.mdl = new PhysicalModel(sampleRate, (int) baseFrameRate);
+        audioOut = 0;
+    }
+
+    protected void uGenerate(float[] channels)
+    {
+        float sample;
+     //   synchronized(lock) {
+
+
+            this.mdl.computeStep();
+            step++;
+
+            // calculate the sample value
+            if(mdl.matExists(listeningPoint[0]))
+            {
+                sample =(float)((mdl.getMatPosition(listeningPoint[0]).y)* 2.);
+
+                /* High pass filter to remove the DC offset */
+                audioOut =(float)( sample - prevSample + 0.95 * audioOut);
+                prevSample = sample;
+            }
+            else
+                sample = 0;
+       // }
+        Arrays.fill( channels, audioOut );
+        currAudio = audioOut;
+    }
+
+}
